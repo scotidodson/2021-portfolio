5fb360c2744fc2a79e1c99bc75e72e16c4395285
Scoti Dodson <scotidodson@Scotis-MacBook-Pro.local>
2021-07-12 06:32:21 -0400

    css updates

---
+++ src/components/Pillars.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Pillars.css';
+
+const Pillars = () => {
+  return (
+   <div className='pillars'>
+       <div className='pillars__column'>
+          <h2>CODE</h2>
+          <p>Lorem ipsum dolor sit amethenderit animi? Eos, illo adipisci? Officiis animi lam debitis. Aut voluptatum repellendus vitae itaque assumenda consequuntur.</p>
+      </div>
+       <div className='pillars__column'>
+          <h2>UX/UI</h2>
+          <p>Lorem ipsum dolor sit amethenderit animi? Eos, illo adipisci? Officiis animi lam debitis. Aut voluptatum repellendus vitae itaque assumenda consequuntur.</p>
+      </div>
+       <div className='pillars__column'>
+          <h2>PROJECTS</h2>
+          <p>Lorem ipsum dolor sit amethenderit animi? Eos, illo adipisci? Officiis animi lam debitis. Aut voluptatum repellendus vitae itaque assumenda consequuntur.</p>
+      </div>
+   </div>
+  );
+};
+
+export default Pillars;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import './Home.css';
 import Hero from '../components/Hero';
-import Pillars from './Pillars';
+import Pillars from '../components/Pillars';
 import Portfolio from '../components/Portfolio'
 import Resume from '../components/Resume';
 import Contact from '../components/Contact';
