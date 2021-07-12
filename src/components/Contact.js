a3683b490f2517045df3ea88e470f7df07b3781f
Scoti Dodson <scotidodson@Scotis-MacBook-Pro.local>
2021-07-12 06:04:00 -0400

    resume timeline css update, added basic contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
 import React from "react";
-// import './Contact.css';
+import './Contact.css';
 
 const Contact = () => {
   return (
-   <div>contact</div>
+   <div className='contact'>
+     <h3>linkedin</h3>
+     <h3>email</h3>
+     <h3>github</h3>
+   </div>
   );
 };
 
