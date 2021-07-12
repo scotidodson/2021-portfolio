e29a41ce63566d9c1d73507797b942424d9c0a9f
Scoti Dodson <scotidodson@Scotis-MacBook-Pro.local>
2021-07-12 09:07:13 -0400

    hero adjustments

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,15 @@ import './Hero.css';
 const Hero = () => {
   return (
     <div className='hero'>
-      <div className="left">
+      <div className="hero__intro">
         <h2>Hi, I'm Scoti.</h2>
         <p>I'm a software engineer with a passion for ux design and project management.</p>
-        <p>I'm a software engineer with a passion for ux design and project management. I'm a software engineer with a passion for ux design and project management.</p>
-        <p>linkedin   github   email</p>
+        <p>Currently @ Taboola.</p>
+        <a href="mailto:scotidodson@gmail.com">
+          <h4>
+            GET IN TOUCH &#8594;
+          </h4>
+        </a>
       </div>
     </div>
   );
