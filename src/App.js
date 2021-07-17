d15f70efa907432d7ccf4e03ba15143cf4ca0302
Scoti Dodson <scotidodson@Scotis-MacBook-Pro.local>
2021-07-17 10:48:53 -0400

    updated logo

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ import PageContainer from './containers/PageContainer';
 import FourOhFour from './containers/FourOhFour';
 // import ProjectPage from './components/ProjectPage';
 // import {PORTFOLIO_DATA} from './data';
+import logo from './assets/striped-penant-logo.PNG';
 
 function App() {
   return (
     <Router>
       <nav className='navigation'>
-        <Link to='/'><div className='logo'></div></Link>
+        <Link to='/'>
+          <img src={logo} alt="scoti dodson logo" className="navigation__logo" />
+        </Link>
         <ul>
             <li><HashLink smooth to='#'>ABOUT</HashLink></li>
             <li><HashLink smooth to='#portfolio'>WORK</HashLink></li>
