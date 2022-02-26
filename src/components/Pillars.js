236b109792701234f6d439d55390e67cfc5cef48
Scoti Dodson <scotidodson@gmail.com>
2022-02-26 17:22:04 -0500

    updated margins and resume data

--- src/components/Pillars.js
+++ src/components/Pillars.js
@@ -6,11 +6,11 @@ const Pillars = () => {
    <div className='pillars'>
        <div className='pillars__column'>
           <h2>CODE</h2>
-          <p>Lorem ipsum dolor sit amethenderit animi? Eos, illo adipisci? Officiis animi lam debitis. Aut voluptatum repellendus vitae itaque assumenda consequuntur.</p>
+          <p>Javascript (E5 + E6) - CSS - SASS - React - Redux - Jest - Node - Rails - Rest APIs - SQL - KQL - DevTools + Debugging - Swift - SDK Integrations - SEO + CWV Best Practices</p>
       </div>
        <div className='pillars__column'>
           <h2>UX / UI</h2>
-          <p>Lorem ipsum dolor sit amethenderit animi? Eos, illo adipisci? Officiis animi lam debitis. Aut voluptatum repellendus vitae itaque assumenda consequuntur.</p>
+          <p></p>
       </div>
        <div className='pillars__column'>
           <h2>PROJECTS</h2>
--- src/data.js
+++ src/data.js
@@ -1,21 +1,35 @@
 export const RESUME_DATA = [{
-        "timeframe":"MAY 2019 - PRESENT",
+        "timeframe": "SEPTEMBER 2021 - PRESENT",
+        "company": "Plaid",
+        "website": "https://www.plaid.com/",
+        "roles": [{
+                "title": "Solutions Engineer",
+                "start": "",
+                "end": "",
+                "responsibilities": ["Provide dedicated technical guidance and integration support to a portfolio of key strategic clients",
+                        "Partner with account managers to strengthen client relationships and drive upsells through consultative solutioning",
+                        "Manage roadmap of integration optimizations and technical deadlines",
+                ]
+        }]
+}, {
+        "timeframe": "MAY 2019 - SEPTEMBER 2021",
         "company": "Taboola",
         "website": "https://www.taboola.com/",
         "roles": [{
                 "title": "Solutions Engineer",
                 "start": "",
                 "end": "",
-                "responsibilities": ["Scope, develop, QA, and launch custom Javascript & CSS integrations for premium publisher clients such as NBC",
-                "Consult with clients on best fit solutions and complex debugging for web and mobile app products",
-                "Develop internal tools (full stack React applications) to optimize workflows and drive efficiency",
-                "Collaborate with Product and R&D on data driven product improvements and feature requests",
-                "As subject matter expert for mobile SDK and Newsroom products, provide ongoing trainings to the Professional Services team and mentor teammates on complex issues to identify product bugs and minimize R&D escalations",
-                "Facilitate A/B testing to optimize partner integrations",
-                "Manage various initiatives such as Tech Talks to drive knowledge sharing and collaboration between teams"]
-            }]
-    },{
-        "timeframe":"MARCH 2019 - MAY 2019",
+                "responsibilities": ["Scoped, developed, and launched custom Javascript & CSS integrations for premium publisher clients such as NBC", 
+                        "Consulted with clients on best fit solutions and complex debugging for web and mobile app products and facilitated A/B testing to optimize product integrations",
+                        "Developed internal tools (full stack React applications) to optimize workflows and drive efficiency",
+                        "Collaborated with Product and R&D on data driven product improvements and client driven feature requests",
+                        "As SME for Newsroom products, developed and delivered trainings to the Professional Services team, scaled our support for the product suite globally, and collaborated on improvements with the product team",
+                        "As SME for Taboola's mobile SDKs, mentored teammates on complex issues to identify product bugs and minimize R&D escalations",
+                        "Managed program of Tech Talks to drive knowledge sharing and collaboration between solutions engineering and account management"
+                ]
+        }]
+}, {
+        "timeframe": "MARCH 2019 - MAY 2019",
         "company": "minds + assembly",
         "website": "https://www.mna.co/",
         "roles": [{
@@ -23,20 +37,21 @@ export const RESUME_DATA = [{
                 "start": "null",
                 "end": "null",
                 "responsibilities": ["Coded pixel perfect products with CSS, HTML, Javascript",
-                    "Consulted with design and production teams on UX/UI best practices and capabilities"]
-            }]
-    },{
-        "timeframe":"SEPTEMBER 2018 - JANUARY 2019",
+                        "Consulted with design and production teams on UX/UI best practices and capabilities"
+                ]
+        }]
+}, {
+        "timeframe": "SEPTEMBER 2018 - JANUARY 2019",
         "company": "Flatiron School",
         "website": "https://www.flatironschool.com/",
         "roles": [{
-                "title":"Student, Web Development Immersive",
+                "title": "Student, Web Development Immersive",
                 "start": "null",
                 "end": "null",
                 "responsibilities": ["describe here"]
-            }]
-    },{
-        "timeframe":"JANUARY 2013 - AUGUST 2018",
+        }]
+}, {
+        "timeframe": "JANUARY 2013 - AUGUST 2018",
         "company": "The Pre-Tech Era of My Career",
         "website": "null",
         "roles": [{
@@ -44,9 +59,9 @@ export const RESUME_DATA = [{
                 "start": "null",
                 "end": "null",
                 "responsibilities": ["Before I pivoted into tech, I held multiple roles in the legal industry. I was primarily focused on project coordination, operational efficiency improvements, and event production."]
-            }]
-    },{
-        "timeframe":"SEPTEMBER 2009 - MAY 2013",
+        }]
+}, {
+        "timeframe": "SEPTEMBER 2009 - MAY 2013",
         "company": "University of Maryland",
         "website": "null",
         "roles": [{
@@ -54,19 +69,19 @@ export const RESUME_DATA = [{
                 "start": "null",
                 "end": "null",
                 "responsibilities": ["something about college here"]
-            }]
+        }]
 }];
 
 export const PORTFOLIO_DATA = [{
-                "title":"Project A",
-                "tech": ["Javascript","Rails API"],
-                "blurb": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci aliquid repudiandae a veniam nesciunt minus? Nihil id magni assumenda earum animi eveniet, suscipit, et, atque doloribus consequatur aut voluptatibus.",
-                "thumbnail": "image",
-                "details": {}
-        },{
-                "title":"Project B",
-                "tech": ["Javascript","Rails API"],
-                "blurb": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci aliquid repudiandae a veniam nesciunt minus? Nihil id magni assumenda earum animi eveniet, suscipit, et, atque doloribus consequatur aut voluptatibus.",
-                "thumbnail": "image",
-                "details": {}
+        "title": "Project A",
+        "tech": ["Javascript", "Rails API"],
+        "blurb": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci aliquid repudiandae a veniam nesciunt minus? Nihil id magni assumenda earum animi eveniet, suscipit, et, atque doloribus consequatur aut voluptatibus.",
+        "thumbnail": "image",
+        "details": {}
+}, {
+        "title": "Project B",
+        "tech": ["Javascript", "Rails API"],
+        "blurb": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci aliquid repudiandae a veniam nesciunt minus? Nihil id magni assumenda earum animi eveniet, suscipit, et, atque doloribus consequatur aut voluptatibus.",
+        "thumbnail": "image",
+        "details": {}
 }]
