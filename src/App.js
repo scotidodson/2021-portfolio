795d53c3578642dea134609a40d9b157303e0fb1
Scoti Dodson <scotidodson@gmail.com>
2022-02-26 21:25:46 -0500

    css updates

--- src/App.js
+++ src/App.js
@@ -8,23 +8,26 @@ import {
 import { HashLink } from 'react-router-hash-link'
 import PageContainer from './containers/PageContainer';
 import FourOhFour from './containers/FourOhFour';
-import ProjectPage from './components/ProjectPage';
-import {PORTFOLIO_DATA} from './data';
+// import ProjectPage from './components/ProjectPage';
+// import {PORTFOLIO_DATA} from './data';
 
 function App() {
   return (
     <Router>
-      <nav className='navigation'>
-        <div className='sd'>
-            <span className='scoti'><Link to='/'>S</Link></span>
-        </div>
-        <ul>
-            <li><HashLink smooth to='#'>ABOUT</HashLink></li>
-            {/* <li><HashLink smooth to='#portfolio'>WORK</HashLink></li> */}
-            <li><HashLink smooth to='#resume'>RESUME</HashLink></li>
-            <li><HashLink smooth to='#contact'>CONTACT</HashLink></li>
-        </ul>
-      </nav>
+      <div>
+        <nav className='navigation'>
+          <div className='sd'>
+              <span className='scoti'><Link to='/'>S</Link></span>
+          </div>
+          <ul>
+              <li><HashLink smooth to='#top'>ABOUT</HashLink></li>
+              {/* <li><HashLink smooth to='#portfolio'>WORK</HashLink></li> */}
+              <li><HashLink smooth to='#resume'>RESUME</HashLink></li>
+              <li><HashLink smooth to='#contact'>CONTACT</HashLink></li>
+          </ul>
+        </nav>
+      </div>
+      <div id="top"></div>
         <Switch>
           {/* <Route path='/work/:id'><ProjectPage /></Route> */}
           <Route exact path='/'><PageContainer/></Route>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,15 +3,15 @@ import '../style/Hero.css';
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id="about" className="hero">
       <div className="hero__intro">
         <h1>Hi, I'm <span>Scoti</span>.</h1>
         <p>I'm a customer-facing engineer focused on helping clients optimize their API integrations. I love the frontend and my favorite projects involve some element of UX or visual design consulting.</p><p>My primary experience is in vanilla <strong>Javascript</strong>&nbsp;(E5&nbsp;+&nbsp;E6), <strong>React</strong>,and <strong>CSS</strong>. I'm always learning&nbsp;--&nbsp;right now I'm studying <strong>Swift</strong> and <strong>UX/UI&nbsp;Design</strong>.</p>
-        <p>Away from the computer, I'm into comedy, books, and video production.</p>
-        <p>Currently: Solutions Engineering @ Plaid</p>
+        <p>Away from the computer, I'm into comedy, books, and board games.</p>
+        <p className="hero__intro--emphasis">Currently: Solutions Engineering @ Plaid</p>
         <a href="mailto:scotidodson@gmail.com">
           <h4>
-            GET IN TOUCH &#8594;
+            GET IN TOUCH &#8680;
           </h4>
         </a>
       </div>
